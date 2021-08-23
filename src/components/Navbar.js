9c20d37f1db5a8c2e366810deadb2fedcb98c53f
Ralph Arvin De Castro <ralpharvin.dc@gmail.com>
2021-08-23 14:53:33 -0400

    added navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Navbar = () => {
-  return <div>This is the navbar</div>
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-center">
+          <h1>ralphDC</h1>
+          <div className="navbar-links">
+            <Link to="/" className="navbar-link">
+              Home
+            </Link>
+            <Link to="/projects/" className="navbar-link">
+              Projects
+            </Link>
+          </div>
+          <div>Social Media</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Navbar
